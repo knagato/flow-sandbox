dc667e191ced057ac824048f029da6975ef78266
knagato <knagato@users.noreply.github.com>
2022-01-30 19:15:16 +0000

    deploy: e7ce26c85cd403041784aa3f893a8f42e333e6b5

---
+++ _next/static/eEe_W73vPXiW8ESiqYTdV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0fa700cd-9ff225fb35032c8b.js","static/chunks/733-75a3ebedd724969d.js","static/chunks/pages/index-1abc75d9eb89ff2a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
